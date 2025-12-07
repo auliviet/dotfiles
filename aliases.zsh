a0ab35d9fbc7401a715621a37b2c63b43cb965b1
Auliviet <112747495+auliviet@users.noreply.github.com>
2025-12-07 13:15:08 +1100

    fix: fix nvm path to use homebrew installation

--- aliases.zsh
+++ aliases.zsh
@@ -19,7 +19,7 @@ alias dockerrm='docker stop $(docker ps -aq) \
 alias glog='git log --graph'
 alias gs='git status -sb'
 
-# Ndode
+# Node
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
